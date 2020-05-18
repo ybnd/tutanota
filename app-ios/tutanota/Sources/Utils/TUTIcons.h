478d8a6c52cac49f840514d494385b0b1aaf1b5d
ivk <ivk@tutao.de>
2020-05-18 11:34:37 +0200

    Remove big extra files from repository

--- app-ios/tutanota/Sources/Utils/TUTIcons.h
+++
@@ -1,3 +0,0 @@
-#import <Foundation/Foundation.h>
-extern NSString * const TUT_ICON_CAMERA;
-extern NSString * const TUT_ICON_FILES;
--- app-ios/tutanota/Sources/Utils/TUTIcons.m
+++
@@ -1,3 +0,0 @@
-#import "TUTIcons.h"
-NSString * const TUT_ICON_CAMERA = @"\uf3f5";
-NSString * const TUT_ICON_FILES = @"\uf19b";
