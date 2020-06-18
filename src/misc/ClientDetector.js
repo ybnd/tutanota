da216ca5bc04cfa413df73794f9ab2b7456ccfae
ivk <ivk@tutao.de>
2020-06-18 11:08:14 +0200

    Bump minimum Chrome version to 45, fix #2096

--- src/misc/ClientDetector.js
+++ src/misc/ClientDetector.js
@@ -376,7 +376,7 @@ class ClientDetector {
 	}
 
 	notOldChrome() {
-		return this.browser !== BrowserType.CHROME || this.browserVersion > 37
+		return this.browser !== BrowserType.CHROME || this.browserVersion > 44
 	}
 
 	canDownloadMultipleFiles(): boolean {
