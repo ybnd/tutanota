e45eb107fb71f7368a5f2bb6f6c2a50c5cd5258f
jom <jom@tutao.de>
2020-09-29 16:32:47 +0200

    add testing files for SendMailModel

---
+++ test/client/mail/SendMailModelTest.js
@@ -0,0 +1,21 @@
+// @flow
+
+o.spec("SendMailModel", () => {
+
+	/* To Test
+	Adding, Removing, Updating recipients
+	Sending emails
+	Saving drafts
+
+	All forms of email initialization
+		New,
+		From Draft,
+		etc
+
+	updating passwords
+	checking for exceptions to be thrown upon saving draft and sending
+
+	updating subject and body
+
+	 */
+})
